ccd2c1a0449c04b0dcf032efd94c03af4494c6a4
sbastian <sbastian@kil-m-03.htw-saarland.de>
2015-06-11 16:42:08 +0200

    ListenDialog erstellt

---
+++ LinList/ListenDialog.cpp
@@ -0,0 +1,18 @@
+/*
+ * ListenDialog.cpp
+ *
+ *  Created on: 11.06.2015
+ *      Author: sbastian
+ */
+
+#include "ListenDialog.h"
+//TODO Implement me Senpai
+ListenDialog::ListenDialog() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ListenDialog::~ListenDialog() {
+	// TODO Auto-generated destructor stub
+}
+
